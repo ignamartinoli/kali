fc84f6682652f51e21c9651e688ffad3fb65125d
ignamartinoli <103866722+ignamartinoli@users.noreply.github.com>
2023-09-18 01:27:06 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,44 +1,44 @@
 #!/bin/sh
 
-# cyan='\033[0;36m'
+cyan='\033[0;36m'
 
-# echo "$cyan-=-=-=-=-=[ Changing root password ]=-=-=-=-=-"
+echo "$cyan-=-=-=-=-=[ Changing root password ]=-=-=-=-=-"
 sudo passwd root
 
-# echo "$cyan-=-=-=-=-=[ Changing user password ]=-=-=-=-=-"
+echo "$cyan-=-=-=-=-=[ Changing user password ]=-=-=-=-=-"
 sudo passwd kali
 
-# echo "$cyan-=-=-=-=-=[ Updating ]=-=-=-=-=-"
-# sudo apt update -qqy && sudo apt upgrade -qqy
-# sudo apt autoremove -qqy
+echo "$cyan-=-=-=-=-=[ Updating ]=-=-=-=-=-"
+sudo apt update -qqy && sudo apt upgrade -qqy
+sudo apt autoremove -qqy
 
-# echo "$cyan-=-=-=-=-=[ Upgrading ]=-=-=-=-=-"
-# sudo apt update -qqy && sudo apt dist-upgrade -qqy
+echo "$cyan-=-=-=-=-=[ Upgrading ]=-=-=-=-=-"
+sudo apt update -qqy && sudo apt dist-upgrade -qqy
 
-# echo "$cyan-=-=-=-=-=[ Installing drivers ]=-=-=-=-=-"
-# sudo apt install realtek-rtl88xxau-dkms -qqy
+echo "$cyan-=-=-=-=-=[ Installing drivers ]=-=-=-=-=-"
+sudo apt install realtek-rtl88xxau-dkms -qqy
 
-# echo "$cyan-=-=-=-=-=[ Adding keymaps ]=-=-=-=-=-"
+echo "$cyan-=-=-=-=-=[ Adding keymaps ]=-=-=-=-=-"
 echo 'sed'
 sed -i '/backward-kill-line/a bindkey '\''^H'\'' backward-kill-word                   # ctrl + delete' "$HOME/.zshrc"
 echo 'xmod'
 echo 'keycode 66 = Escape' > "$HOME/.Xmodmap"
 xmodmap "$HOME/.Xmodmap"
 
-# echo "$cyan-=-=-=-=-=[ Installing Nerd Font ]=-=-=-=-=-"
-# fonts="$HOME/.local/share/fonts"
-# wget -qO "$HOME/FiraCode.zip" 'github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip'
-# [ -d "$fonts" ] || mkdir -p "$fonts" && unzip -od "$fonts" "$HOME/FiraCode.zip"
-# rm "$HOME/FiraCode.zip"
-# fc-cache -fv
+echo "$cyan-=-=-=-=-=[ Installing Nerd Font ]=-=-=-=-=-"
+fonts="$HOME/.local/share/fonts"
+wget -qO "$HOME/FiraCode.zip" 'github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip'
+[ -d "$fonts" ] || mkdir -p "$fonts" && unzip -od "$fonts" "$HOME/FiraCode.zip"
+rm "$HOME/FiraCode.zip"
+fc-cache -fv
 
-# echo "$cyan-=-=-=-=-=[ Installing Neovim ]=-=-=-=-=-"
-# wget -qO "$HOME/nvim-linux64.tar.gz" 'github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz'
-# tar xzf "$HOME/nvim-linux64.tar.gz" -C "$HOME"
-# rm "$HOME/nvim-linux64.tar.gz"
-# sudo ln -s "$HOME/nvim-linux64/bin/nvim" '/usr/local/bin/nvim'
-# sudo apt install npm ripgrep -qqy
-# git clone 'https://github.com/NvChad/NvChad' "$HOME/.config/nvim" --depth 1
+echo "$cyan-=-=-=-=-=[ Installing Neovim ]=-=-=-=-=-"
+wget -qO "$HOME/nvim-linux64.tar.gz" 'github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz'
+tar xzf "$HOME/nvim-linux64.tar.gz" -C "$HOME"
+rm "$HOME/nvim-linux64.tar.gz"
+sudo ln -s "$HOME/nvim-linux64/bin/nvim" '/usr/local/bin/nvim'
+sudo apt install npm ripgrep -qqy
+git clone 'https://github.com/NvChad/NvChad' "$HOME/.config/nvim" --depth 1
 # TODO: sed "$HOME/.config/nvim/lua/custom/configs/overrides.lua"
 
-# echo "$cyan-=-=-=-=-=[ SETUP FINISHED ]=-=-=-=-=-"
+echo "$cyan-=-=-=-=-=[ SETUP FINISHED ]=-=-=-=-=-"
