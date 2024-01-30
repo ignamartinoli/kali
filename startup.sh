b7d45ac022d9e4eb0bc3ae22fba0e53ad9d9d4e4
ignamartinoli <103866722+ignamartinoli@users.noreply.github.com>
2024-01-30 01:35:10 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,6 +4,8 @@
 
 [ "$(id -u)" -eq 0 ] || { tput setaf 1; echo "You must run this as root"; tput sgr0; exit 1; }
 
+user='/home/kali'
+
 tput setaf 4; echo '-=-=-=-=-=[ Changing passwords ]=-=-=-=-=-'; tput sgr0
 echo 'New password: \c'
 read -r password
@@ -21,24 +23,24 @@ tput setaf 4; echo '-=-=-=-=-=[ Installing drivers ]=-=-=-=-=-'; tput sgr0
 apt install realtek-rtl88xxau-dkms -qqy
 
 tput setaf 4; echo '-=-=-=-=-=[ Adding keymaps ]=-=-=-=-=-'; tput sgr0
-sed -i '/backward-kill-line/a bindkey '\''^?'\'' backward-kill-word                   # ctrl + delete' "$HOME/.zshrc"
-echo 'keycode 66 = Escape' > "$HOME/.Xmodmap"
-xmodmap "$HOME/.Xmodmap"
+sed -i '/backward-kill-line/a bindkey '\''^?'\'' backward-kill-word                   # ctrl + delete' "$user/.zshrc"
+echo 'keycode 66 = Escape' > "$user/.Xmodmap"
+xmodmap "$user/.Xmodmap"
 
 tput setaf 4; echo '-=-=-=-=-=[ Installing Nerd Font ]=-=-=-=-=-'; tput sgr0
-fonts="$HOME/.local/share/fonts"
-wget -qO "$HOME/FiraCode.zip" 'github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip'
-[ -d "$fonts" ] || mkdir -p "$fonts" && unzip -od "$fonts" "$HOME/FiraCode.zip"
-rm "$HOME/FiraCode.zip"
+fonts="$user/.local/share/fonts"
+wget -qO "$user/FiraCode.zip" 'github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip'
+[ -d "$fonts" ] || mkdir -p "$fonts" && unzip -od "$fonts" "$user/FiraCode.zip"
+rm "$user/FiraCode.zip"
 fc-cache -fv
 
 tput setaf 4; echo '-=-=-=-=-=[ Installing Neovim ]=-=-=-=-=-'; tput sgr0
-wget -qO "$HOME/nvim-linux64.tar.gz" 'github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz'
-tar xzf "$HOME/nvim-linux64.tar.gz" -C "$HOME"
-rm "$HOME/nvim-linux64.tar.gz"
-ln -s "$HOME/nvim-linux64/bin/nvim" '/usr/local/bin/nvim'
+wget -qO "$user/nvim-linux64.tar.gz" 'github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz'
+tar xzf "$user/nvim-linux64.tar.gz" -C "$user"
+rm "$user/nvim-linux64.tar.gz"
+ln -s "$user/nvim-linux64/bin/nvim" '/usr/local/bin/nvim'
 apt install npm ripgrep -qqy
-git clone 'https://github.com/NvChad/NvChad' "$HOME/.config/nvim" --depth 1
+git clone 'https://github.com/NvChad/NvChad' "$user/.config/nvim" --depth 1
 # TODO: sed "$HOME/.config/nvim/lua/custom/configs/overrides.lua"
 
 tput setaf 4; echo '-=-=-=-=-=[ SETUP FINISHED ]=-=-=-=-=-'; tput sgr0
