cc6c4275d9346ac1523a0397983f42bf683d3468
igna_martinoli <ignamartinoli@protonmail.com>
2024-07-17 18:38:12 -0300

    improves

--- startup.sh
+++ startup.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
+# TODO: install pip?, rm gnome-software?, install golang?, install wallpapers?, install docker?, install zmap?, install hcxtools?, install bettercap?, install eaphammer?, install gowitness?, install man-spider?, install bloodhound?, install PCredz?, install EavesARP?, install CrackMapExec?, install Impacket?
+# TODO: initializing metasploit database?
+# TODO: unzipping RockYou
+# TODO: BurpSuite Pro?
+# TODO: Firefox Add-Ons?
+# TODO: https://github.com/blacklanternsecurity/kali-setup-script/blob/master/kali-setup-script.sh
+
+message() {
+	message="$1"
+	tput setaf 2; echo "-=-=-=-=-=[ $message ]=-=-=-=-=-"; tput sgr0
+}
+
 tidy() {
 	# BUG: if there is a problem with a package, dpkg fucks all the process up
 	# sudo dpkg --configure -a
-	rm -rf "$HOME/FiraCode.zip"
-	rm -f "$HOME/nvim-linux64.tar.gz"
+	rm -rf "$HOME/FiraCode.zip" "$HOME/nvim-linux64.tar.gz"
 }
 
 reset() {
 	tidy
  	rm -f "$HOME/.Xmodmap"
-	rm -rf "$HOME/nvim-linux64"
- 	rm -rf "$HOME/.config/nvim"
-	rm -rf "$HOME/.local/share/nvim"
-	rm -rf "$HOME/.local/state/nvim"
-	rm -rf "$HOME/.cache/nvim"
+	rm -rf "$HOME/nvim-linux64" "$HOME/.config/nvim" "$HOME/.local/share/nvim" "$HOME/.local/state/nvim" "$HOME/.cache/nvim"
 	sudo rm -f '/usr/local/bin/nvim'
 }
 
@@ -29,7 +36,7 @@ sudo -v || { echo 'Incorrect password.'; exit 1; }
 trap error 1 2 3 6
 reset
 
-tput setaf 2; echo '-=-=-=-=-=[ Changing passwords ]=-=-=-=-=-'; tput sgr0
+message 'Changing passwords'
 echo 'New password: \c'
 stty -echo
 read -r password
@@ -38,40 +45,45 @@ echo
 echo "root:$password" | sudo chpasswd
 echo "kali:$password" | sudo chpasswd
 
-tput setaf 2; echo '-=-=-=-=-=[ Updating ]=-=-=-=-=-'; tput sgr0
+message 'Updating'
+export DEBIAN_FRONTEND='noninteractive'
 sudo apt update -qqy && sudo apt upgrade -qqy
 sudo apt autoremove -qqy
 
+message 'Upgrading'
 tput setaf 2; echo '-=-=-=-=-=[ Upgrading ]=-=-=-=-=-'; tput sgr0
 sudo apt update -qqy && sudo apt dist-upgrade -qqy
 
-tput setaf 2; echo '-=-=-=-=-=[ Installing kernel headers ]=-=-=-=-=-'; tput sgr0
+message 'Installing kernel headers'
 sudo apt install "linux-headers-$(uname -r)"
 
-tput setaf 2; echo '-=-=-=-=-=[ Installing drivers ]=-=-=-=-=-'; tput sgr0
+message 'Installing drivers'
 sudo apt install realtek-rtl88xxau-dkms -qqy
 
-tput setaf 2; echo '-=-=-=-=-=[ Adding keymaps ]=-=-=-=-=-'; tput sgr0
+message 'Adding keymaps'
 grep -q '^bindkey '\''^?'\''' "$HOME/.zshrc" || sed -i '/backward-kill-line/a bindkey '\''^?'\'' backward-kill-word                   # ctrl + delete' "$HOME/.zshrc"
 echo 'keycode 66 = Escape' > "$HOME/.Xmodmap"
 xmodmap "$HOME/.Xmodmap"
 
-tput setaf 2; echo '-=-=-=-=-=[ Installing Nerd Font ]=-=-=-=-=-'; tput sgr0
+message 'Installing Nerd Font'
 # TODO: directory="$(mktemp -d)"
 fonts="$HOME/.local/share/fonts"
-wget -qO "$HOME/FiraCode.zip" 'github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip'
+wget -qO "$HOME/FiraCode.zip" 'https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip'
 mkdir -p "$fonts" && unzip -od "$fonts" "$HOME/FiraCode.zip"
 fc-cache -fv
 
-tput setaf 2; echo '-=-=-=-=-=[ Installing Neovim ]=-=-=-=-=-'; tput sgr0
-wget -qO "$HOME/nvim-linux64.tar.gz" 'github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz'
+message 'Installing Neovim'
+wget -qO "$HOME/nvim-linux64.tar.gz" 'https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz'
 tar xzf "$HOME/nvim-linux64.tar.gz" -C "$HOME"
-sudo ln -s "$HOME/nvim-linux64/bin/nvim" '/usr/local/bin/nvim'
+sudo ln -sfn "$HOME/nvim-linux64/bin/nvim" '/usr/local/bin/nvim'
 
 sudo apt install npm ripgrep -qqy
-git clone https://github.com/LazyVim/starter "$HOME/.config/nvim"
+git clone 'https://github.com/LazyVim/starter' "$HOME/.config/nvim"
 rm -rf "$HOME/.config/nvim/.git"
 # TODO: sed "$HOME/.config/nvim/lua/custom/configs/overrides.lua"
+
+message 'Setting wallpaper'
+xfconf-query -c 'xfce4-desktop' -p '/backdrop/screen0/monitorVirtual1/workspace0/last-image' -s '/usr/share/backgrounds/kali-16x9/kali-metal-dark.png'
 
 tput setaf 2; echo '-=-=-=-=-=[ Setup finished ]=-=-=-=-=-'; tput sgr0
 tidy
