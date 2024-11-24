cad2ec1220b5ec1553442e1e304c28cdebe4d4d7
igna_martinoli <ignamartinoli@protonmail.com>
2024-11-24 17:17:21 -0300

    Multi-editor installation and refactoring

--- startup.sh
+++ startup.sh
@@ -8,31 +8,30 @@
 # TODO: https://github.com/blacklanternsecurity/kali-setup-script/blob/master/kali-setup-script.sh
 
 message() {
-	tput setaf 2; echo "-=-=-=-=-=[ $1 ]=-=-=-=-=-"; tput sgr0
-}
-
-tidy() {
-	# BUG: if there is a problem with a package, dpkg fucks all the process up
-	# sudo dpkg --configure -a
-	rm -rf "$HOME/FiraCode.zip" "$HOME/nvim-linux64.tar.gz"
+	tput setaf 2
+	echo "-=-=-=-=-=[ $1 ]=-=-=-=-=-"
+	tput sgr0
 }
 
 reset() {
-	tidy
- 	rm -f "$HOME/.Xmodmap"
-	rm -rf "$HOME/nvim-linux64" "$HOME/.config/nvim" "$HOME/.local/share/nvim" "$HOME/.local/state/nvim" "$HOME/.cache/nvim"
- 	sudo rm -f '/usr/local/bin/hx'
-  	rm -rf "${XDG_CONFIG_HOME:-$HOME/.config}/helix"
+	rm -f "$HOME/.Xmodmap"
+	sudo rm -f '/usr/local/bin/hx'
+	rm -rf "${XDG_CONFIG_HOME:-$HOME/.config}/helix"
 }
 
 error() {
 	stty echo
-	tput setaf 1; echo '-=-=-=-=-=[ Error ]=-=-=-=-=-'; tput sgr0
+	tput setaf 1
+	echo '-=-=-=-=-=[ Error ]=-=-=-=-=-'
+	tput sgr0
 	reset
 	exit 1
 }
 
-sudo -v || { echo 'Incorrect password.'; exit 1; }
+sudo -v || {
+	echo 'Incorrect password.'
+	exit 1
+}
 trap error 1 2 3 6
 reset
 
@@ -79,16 +78,34 @@ wget -qO "$playground/FiraCode.zip" 'https://github.com/ryanoasis/nerd-fonts/rel
 mkdir -p "$fonts" && unzip -od "$fonts" "$playground/FiraCode.zip"
 fc-cache -fv
 
-message 'Installing Helix'
-version='24.07-x86_64-linux'
-wget -qO "$playground/helix.tar.xz" "https://github.com/helix-editor/helix/releases/latest/download/helix-$version.tar.xz" 
-tar -xf "$playground/helix.tar.xz" -C "$playground"
-sudo mv "$playground/helix-$version/hx" '/usr/local/bin'
-mv -f "$playground/helix-$version/runtime" "${XDG_CONFIG_HOME:-$HOME/.config}/helix"
-wget -qO "${XDG_CONFIG_HOME:-$HOME/.config}/helix/config.toml" 'https://raw.githubusercontent.com/ignamartinoli/kali/master/config.toml'
+message 'Installing editor'
+while true; do
+	printf "1) Neovim\n2) Helix\n\nSelect: "
+	read -r choice
+
+	case "$choice" in
+		1)
+			sudo apt install 'neovim'
+			git clone 'https://github.com/LazyVim/starter' "${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
+			rm -rf "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/.git"
+			;;
+		2)
+			version='24.07-x86_64-linux'
+			wget -qO "$playground/helix.tar.xz" "https://github.com/helix-editor/helix/releases/latest/download/helix-$version.tar.xz"
+			tar -xf "$playground/helix.tar.xz" -C "$playground"
+			sudo mv "$playground/helix-$version/hx" '/usr/local/bin'
+			mv -f "$playground/helix-$version/runtime" "${XDG_CONFIG_HOME:-$HOME/.config}/helix"
+			wget -qO "${XDG_CONFIG_HOME:-$HOME/.config}/helix/config.toml" 'https://raw.githubusercontent.com/ignamartinoli/kali/master/config.toml'
+			;;
+		*)
+			continue
+			;;
+	esac
+
+	break
+done
 
 message 'Setting wallpaper'
 xfconf-query -c 'xfce4-desktop' -p '/backdrop/screen0/monitorVirtual1/workspace0/last-image' -s '/usr/share/backgrounds/kali-16x9/kali-metal-dark.png'
 
-tput setaf 2; echo '-=-=-=-=-=[ Setup finished ]=-=-=-=-=-'; tput sgr0
-tidy
+message 'Setup finished'
